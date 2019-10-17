152ad674fe427b5e03870fe14409310948bc1f22
dragos <asd>
2019-10-17 19:08:29 +0000

    - add script to start idea

--- idea.sh
+++ idea.sh
@@ -1,1 +1,11 @@
-docker run -it --rm
+mkdir -p ~/Projects
+mkdir -p ~/.config-vol/.Idea
+mkdir -p ~/.config-vol/.Idea.share
+
+docker run -it --rm \
+  -e DISPLAY=:0 \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v ~/Projects:/home/dragos/IdeaProjects \
+  -v ~/.config-vol/.Idea:/home/dragos/.IntelliJIdea2019.2
+  -v ~/.config-vol/.Idea.share:/home/dragos/.local/share/JetBrains
+  draducanu/idea
