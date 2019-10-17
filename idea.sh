aba04fb028340f62cdcfa8fe4b97399b87ed3189
Dragos Raducanu <raducanudragos12@gmail.com>
2019-10-18 00:02:37 +0300

    optimize dockerfiles

--- idea.sh
+++ idea.sh
@@ -8,4 +8,5 @@ docker run -it --rm -e DISPLAY=:0 \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v ~/Projects:/home/dragos/IdeaProjects \
   -v ~/.config-vol/.Idea:/home/dragos/.IntelliJIdea2019.2 \
+  -v ~/.config-vol/.Idea.share:/home/dragos/.local/share/JetBrains \
   draducanu/idea
