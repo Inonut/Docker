e73d2b573c911cff7dc10636ebb7856885e20b2b
dragos <asd>
2019-10-18 21:48:27 +0000

    fix idea.sh

--- idea.sh
+++ idea.sh
@@ -7,5 +7,5 @@ docker volume create tools-config
 docker run -it --rm -e DISPLAY=:0 \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v ~/Projects:/home/dragos/IdeaProjects \
-  -v tools-config:/home
+  -v tools-config:/home \
   draducanu/archlinux-tools
