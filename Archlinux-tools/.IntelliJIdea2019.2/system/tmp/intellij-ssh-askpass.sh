32141b86c3542f0847f1169def5f616f229e0a78
dragos <ads>
2019-10-26 20:35:53 +0000

    config idea settings

--- Archlinux-tools/.IntelliJIdea2019.2/system/tmp/intellij-ssh-askpass.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-"/usr/lib/jvm/java-13-openjdk/bin/java" -cp "/opt/intellij-idea-ultimate-edition/plugins/git4idea/lib/git4idea-rt.jar:/opt/intellij-idea-ultimate-edition/lib/xmlrpc-2.0.1.jar:/opt/intellij-idea-ultimate-edition/lib/commons-codec-1.13.jar:/opt/intellij-idea-ultimate-edition/lib/util.jar" org.jetbrains.git4idea.nativessh.GitNativeSshAskPassApp "$@"
