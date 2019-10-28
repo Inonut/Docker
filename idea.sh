337415e755035450218944d83067071c5f3d4db7
dragos <asd>
2019-10-28 15:18:22 +0000

    export idea config

--- idea.sh
+++ idea.sh
@@ -30,12 +30,26 @@ function runIdea() {
     draducanu/archlinux-tools
 }
 
+function exportIdeaConfig() {
+  root=~/IdeaProjects/Docker/Archlinux-tools/
+  cp -fr ~/.IntelliJIdea2019.2/ "$root"
+
+  eval=$(find "$root" -iname eval 2>/dev/null | grep -e \..*/config/eval)
+  rm -fr "$eval"
+
+  statistics=$(find "$root" -iname *statistics* 2>/dev/null | grep -e \..*/config/.*\.xml)
+  rm -fr "$statistics"
+
+  option=$(find "$root" -iname 'other.xml' 2>/dev/null | grep -e \..*/config/options/other\.xml)
+  sed -i.bak '/evlsprt*/d' "$option"
+}
+
 case $1 in
    "build")
       buildArchlinuxUser
       buildArchlinuxGui
       buildArchlinuxTools
       ;;
-   "export") echo TODO export intellij settings from container;;
+   "export") exportIdeaConfig;;
    *) runIdea;;
 esac
