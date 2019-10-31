47a3d5092a6a8b2db79d8e2b44188faee71f8cd3
developer <asd>
2019-10-31 21:47:37 +0000

    add standard config

--- idea.sh
+++ idea.sh
@@ -13,15 +13,28 @@ function runIdea() {
   docker run -it --rm -e DISPLAY=:0 \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v ~/Projects:/home/developer/IdeaProjects \
-    -v ~/.IntelliJIdea2019.2:/home/developer/.IntelliJIdea2019.2 \
-    --net=host \
     draducanu/idea
+}
+
+function exportIdeaConfig() {
+  root=~/IdeaProjects/Docker/Alpine-tools/
+  cp -fr ~/.IntelliJIdea2019.2/ "$root"
+
+  eval=$(find "$root" -iname eval 2>/dev/null | grep -e \..*/config/eval)
+  rm -fr "$eval"
+
+  statistics=$(find "$root" -iname *statistics* 2>/dev/null | grep -e \..*/config/.*\.xml)
+  rm -fr "$statistics"
+
+  option=$(find "$root" -iname 'other.xml' 2>/dev/null | grep -e \..*/config/options/other\.xml)
+  sed -i.bak '/evlsprt*/d' "$option"
 }
 
 case $1 in
    "build")
       buildAlpineTools
       ;;
+    "export") exportIdeaConfig;;
    *) runIdea;;
 esac
 
