92519ea303669100a10b5872b63baadb24734672
Docker Dev <you@example.com>
2020-03-14 18:27:50 +0000

    add install script

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+##### Scipt to install idea on docker in arch linux distro ######
+# asume that you already have docker installed
+
+# install ansible if not present
+sudo pacman --noconfirm -Sy ansible python python-pip
+sudo pip install docker
+
+# add idea yml playbook
+mkdir -p ~/.ansible/idea
+
+cat <<EOF > ~/.ansible/idea/idea.yml
+---
+- name: intellij idea
+  hosts: localhost
+  vars:
+    idea_image: "draducanu/arch-idea"
+    idea_container: "idea-container"
+  tasks:
+    - name: open idea
+      docker_container:
+        name: "{{idea_container}}"
+        image: "{{idea_image}}"
+        pull: true
+        env:
+          DISPLAY: ":0"
+        volumes:
+          - /tmp/.X11-unix:/tmp/.X11-unix
+          - ~/IdeaProjects:/home/developer/IdeaProjects
+
+EOF
+
+# add terminal commad
+echo "alias idea=\"ansible-playbook ~/.ansible/idea/idea.yml\"" >> ~/.bashrc
+source ~/.bashrc
